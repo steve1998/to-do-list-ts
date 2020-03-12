f7c455dfa603aaaae864d4bb1fe6998f2b9c26d4
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-03-12 13:44:08 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.port || '3000'
+const port = process.env.PORT || '3000'
 let app = express()
 
 app.use(express.static(__dirname))
