9a9fce0ac85571e79ddeb65adce13c82f4768693
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-03-12 13:03:45 -0700

    Implement Express

---
+++ server.ts
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.port || '8080'
+let app = express()
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/ping', function (req, res) {
+    return res.send('ping');
+})
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log('Listening on port 8080')
+})
