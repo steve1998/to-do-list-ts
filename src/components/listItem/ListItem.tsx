5abb5fc4cd6e5c81a675bbff0e9b5a8087e90cfe
Nicholas Steven Darmawan <nicholassteven998@gmail.com>
2020-03-12 13:33:32 -0700

    Remove Unnecessary Code and Add Slugignore

--- src/components/listItem/ListItem.tsx
+++ src/components/listItem/ListItem.tsx
@@ -1,8 +1,5 @@
-import React, { useState, Fragment } from 'react'
-
-import { Button, Input } from 'antd'
+import React from 'react'
 import { Typography } from 'antd'
-import { Col } from 'antd'
 
 const { Text } = Typography
 
